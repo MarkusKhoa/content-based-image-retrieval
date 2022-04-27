b4efc990bdf3e315c80b79bfb0da61293ef7223e
MarkusKhoa <dangkhoa20006@gmail.com>
2022-04-27 15:33:38 +0700

    feature extraction method

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,14 +1,26 @@
 import tensorflow as tf
+import os
+import numpy as np
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from utils import folder_to_images
 
+class FeatureExtractor:
+    def __init__(self):
+        base_model = ResNet50(weights = "imagenet", include_top = False)
+        x = base_model.output
+        x = tf.keras.layers.GlobalAveragePooling2D()(x)
+        self.model = tf.keras.Model(inputs = base_model.input, outputs = x)
+    def call(self, input_img):
+        x = preprocess_input(input_img)
+        feature = self.model.predict(x)
+        return feature
 
-def feature_extractor(input_img):
-    x = tf.keras.applications.vgg19.preprocess_input(input_img)
-    base_model = tf.keras.applications.vgg19.VGG19(weights = "imagenet",
-                                                   include_top = False)
-    
-    x = base_model(x)
-    x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    model = tf.keras.Model(inputs = input_img, outputs = x)
-    feature = model.predict(x)
-    
-    return feature
+imgs_feature = []
+paths_feature = []
+FE = FeatureExtractor()
+
+root_images_path = "./images/"
+saved_features_path = "./feature/"
+os.makedirs(saved_features_path, exist_ok = True)
+categories = ["animal", "country", "furniture", "plant", "scenery"]
+img_size = (224, 224)
